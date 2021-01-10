8db47f61dc8b5c87e2f9a36332c66d6154e609e3
Theodore Pauw <theodorepauw@outlook.com>
2021-01-10 02:11:32 +0200

    Dirty hardcoding. Not a fan of this one. 🛂

---
+++ src/twenty20/day04.rs
@@ -0,0 +1,46 @@
+use std::io::{self, Write};
+const INPUT: &str = include_str!("./inputs/04.txt");
+const REQ_FIELDS: [&str; 7] = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+
+pub fn solve() -> crate::util::Result<()> {
+    let year_valid = |year: &str, min: &str, max: &str| -> bool {
+        year.len() == 4 && year >= min && year <= max
+    };
+    let height_valid = |height: &str| -> bool {
+        let unit = &height[height.len() - 2..];
+        let h = &height[..height.len() - 2];
+        (unit == "cm" && h >= "150" && h <= "193") || (unit == "in" && h >= "59" && h <= "76")
+    };
+    let eye_colors = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
+    let hair_color_valid = |code: &str| matches!(code.strip_prefix("#"), Some(clr) if clr.len() == 6 && i64::from_str_radix(clr, 16).is_ok());
+
+    let (p1, p2) = INPUT.split("\n\n").fold((0, 0), |(p1, p2), creds| {
+        let (a, b) = creds
+            .split_whitespace()
+            .map(|s| (&s[0..3], &s[4..]))
+            .map(|(field, value)| match field {
+                "byr" => (true, year_valid(value, "1920", "2002")),
+                "iyr" => (true, year_valid(value, "2010", "2020")),
+                "eyr" => (true, year_valid(value, "2020", "2030")),
+                "hgt" => (true, value.len() > 3 && height_valid(value)),
+                "hcl" => (true, hair_color_valid(value)),
+                "ecl" => (true, eye_colors.contains(&value)),
+                "pid" => (true, value.len() == 9 && value.parse::<i64>().is_ok()),
+                _ => (false, false),
+            })
+            .fold((0, 0), |(p1_valid, p2_valid), (a, b)| {
+                (
+                    p1_valid + if a { 1 } else { 0 },
+                    p2_valid + if b { 1 } else { 0 },
+                )
+            });
+
+        (
+            p1 + if a == REQ_FIELDS.len() { 1 } else { 0 },
+            p2 + if b == REQ_FIELDS.len() { 1 } else { 0 },
+        )
+    });
+
+    writeln!(io::stdout(), "Day 04 Part 1: {}\nDay 04 Part 2: {}", p1, p2)?;
+    Ok(())
+}
