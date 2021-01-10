9e4d857a7f295e471032f5ca5bd4c5ec408d7e9f
Theodore Pauw <theodorepauw@outlook.com>
2021-01-10 19:23:14 +0200

    Love a bit of dynamic programming! 🧨

--- src/twenty20/day10.rs
+++ src/twenty20/day10.rs
@@ -1,15 +1,16 @@
 use std::collections::HashMap;
+use std::io::{self, Write};
 
-const INPUT: &str = include_str!("../inputs/10.txt");
+const INPUT: &str = include_str!("./inputs/10.txt");
 
-fn main() {
+pub fn solve() -> crate::util::Result<()> {
     let mut adapters: Vec<usize> = INPUT
         .lines()
-        .map(|s| s.parse::<usize>().expect("joltage in incorrect format!"))
-        .collect();
+        .map(|s| s.parse::<usize>())
+        .collect::<Result<_, _>>()?;
     adapters.push(0);
     adapters.sort_unstable();
-    adapters.push(adapters.last().expect("no last element")+3);
+    adapters.push(adapters.last().expect("no last element") + 3);
 
     let (one, three) =
         adapters
@@ -20,27 +21,26 @@ fn main() {
                 _ => unreachable!(),
             });
 
-    println!("Day 10 Part 1: {}", one * three);
-    
     let mut paths: HashMap<usize, usize> = HashMap::new();
-    let arrangements = dynamic_count(0, &mut paths, &adapters);
+    let (p1, p2) = (one * three, dynamic_count(0, &mut paths, &adapters));
 
-    println!("Day 10 Part 2: {}", arrangements);
+    writeln!(io::stdout(), "Day 10 Part 1: {}\nDay 10 Part 2: {}", p1, p2)?;
+    Ok(())
 }
 
 fn dynamic_count(i: usize, paths: &mut HashMap<usize, usize>, chain: &[usize]) -> usize {
     if let Some(&p) = paths.get(&i) {
-        return p // path already evaluated
-    } else if i == chain.len()-1 {
-        return 1 // only 1 way to get to the end
+        return p; // path already evaluated
+    } else if i == chain.len() - 1 {
+        return 1; // only 1 way to get to the end
     }
 
     let mut count = 0;
-    for j in i+1..i+4 {
-        if j > chain.len()-1 {
-            break
+    for j in i + 1..i + 4 {
+        if j > chain.len() - 1 {
+            break;
         }
-        if chain[j]-chain[i] <= 3 {
+        if chain[j] - chain[i] <= 3 {
             count += dynamic_count(j, paths, chain);
         }
     }
