aeb1e7826b2743a95a75b52a198a446c05873a7f
Theodore Pauw <theodorepauw@outlook.com>
2021-01-10 23:44:49 +0200

    Minor refactor before rewrite. 🔬

--- src/twenty20/day20.rs
+++ src/twenty20/day20.rs
@@ -36,21 +36,25 @@ pub fn solve() -> util::Result<()> {
         .ok_or("part 2 is whack")?
         .id;
     let mut top_left = tiles.remove(&tl_id).expect("no ref tile"); // 1st corner found is made top left.
-    while !(edges.get(&top_left.rows[0]).map_or(0, |e| e.len())
-        + edges.get(&rev(&top_left.rows[0])).map_or(0, |e| e.len())
+    while !(edges.get(&top_left.grid.rows[0]).map_or(0, |e| e.len())
+        + edges
+            .get(&rev(&top_left.grid.rows[0]))
+            .map_or(0, |e| e.len())
         == 1
-        && edges.get(&top_left.cols[0]).map_or(0, |e| e.len())
-            + edges.get(&rev(&top_left.cols[0])).map_or(0, |e| e.len())
+        && edges.get(&top_left.grid.cols[0]).map_or(0, |e| e.len())
+            + edges
+                .get(&rev(&top_left.grid.cols[0]))
+                .map_or(0, |e| e.len())
             == 1)
     {
-        writeln!(io::stdout(), "{}\n", top_left.rows.join("\n"))?;
-        top_left.turn_left();
+        writeln!(io::stdout(), "{}\n", top_left.grid.rows.join("\n"))?;
+        top_left.grid.turn_left();
     }
     img.push(top_left);
 
     for x in 1..144 {
         if x % 12 == 0 {
-            let e = &img[x - 12].rows[9]; // thus try to match bottom side of anchored tile
+            let e = &img[x - 12].grid.rows[9]; // thus try to match bottom side of anchored tile
             println!("{} {:?}{:?}", x, edges.get(e), edges.get(&rev(e)));
             let id: usize = *edges
                 .get(e)
@@ -61,18 +65,18 @@ pub fn solve() -> util::Result<()> {
                 .ok_or("no id")?;
 
             let mut next_tile = tiles.remove(&id).expect("no ref tile");
-            while &next_tile.rows[0] != e {
+            while &next_tile.grid.rows[0] != e {
                 println!("matching {}", next_tile.id);
-                if &rev(&next_tile.rows[0]) == e {
+                if &rev(&next_tile.grid.rows[0]) == e {
                     println!("flipping h");
-                    next_tile.flip_h();
+                    next_tile.grid.flip_h();
                 } else {
-                    next_tile.turn_left()
+                    next_tile.grid.turn_left()
                 }
             }
             img.push(next_tile);
         } else {
-            let e = &img[x - 1].cols[9]; // thus try to match right side of anchored tile
+            let e = &img[x - 1].grid.cols[9]; // thus try to match right side of anchored tile
             writeln!(
                 io::stdout(),
                 "{} {:?}{:?}",
@@ -89,13 +93,13 @@ pub fn solve() -> util::Result<()> {
                 .ok_or("no id")?;
 
             let mut next_tile = tiles.remove(&id).expect("no ref tile");
-            while &next_tile.cols[0] != e {
+            while &next_tile.grid.cols[0] != e {
                 writeln!(io::stdout(), "matching {}", next_tile.id)?;
-                if &rev(&next_tile.cols[0]) == e {
+                if &rev(&next_tile.grid.cols[0]) == e {
                     println!("flipping v");
-                    next_tile.flip_v()
+                    next_tile.grid.flip_v()
                 } else {
-                    next_tile.turn_left()
+                    next_tile.grid.turn_left()
                 }
             }
             img.push(next_tile);
@@ -112,8 +116,30 @@ pub fn solve() -> util::Result<()> {
 
 struct Tile {
     id: usize,
+    grid: Grid,
+}
+
+struct Grid {
     rows: Vec<String>,
     cols: Vec<String>,
+}
+
+impl Grid {
+    fn turn_left(&mut self) {
+        let new_cols = self.rows.iter().map(|s| rev(s)).collect();
+        self.rows = self.cols.clone().into_iter().rev().collect();
+        self.cols = new_cols;
+    }
+
+    fn flip_v(&mut self) {
+        self.rows = self.rows.clone().into_iter().rev().collect();
+        self.cols = self.cols.clone().into_iter().map(|s| rev(&s)).collect();
+    }
+
+    fn flip_h(&mut self) {
+        self.cols = self.cols.clone().into_iter().rev().collect();
+        self.rows = self.rows.clone().into_iter().map(|s| rev(&s)).collect();
+    }
 }
 
 impl std::str::FromStr for Tile {
@@ -135,35 +161,22 @@ impl std::str::FromStr for Tile {
             },
         );
 
-        Ok(Tile { id, rows, cols })
+        Ok(Tile {
+            id,
+            grid: Grid { rows, cols },
+        })
     }
 }
 
 impl Tile {
     fn edges(&self) -> impl Iterator<Item = &String> {
         let (t, b, l, r) = (
-            iter::once(&self.rows[0]),
-            iter::once(&self.rows[9]),
-            iter::once(&self.cols[0]),
-            iter::once(&self.cols[9]),
+            iter::once(&self.grid.rows[0]),
+            iter::once(&self.grid.rows[9]),
+            iter::once(&self.grid.cols[0]),
+            iter::once(&self.grid.cols[9]),
         );
         t.chain(b).chain(l).chain(r)
-    }
-
-    fn turn_left(&mut self) {
-        let new_cols = self.rows.iter().map(|s| rev(s)).collect();
-        self.rows = self.cols.clone().into_iter().rev().collect();
-        self.cols = new_cols;
-    }
-
-    fn flip_v(&mut self) {
-        self.rows = self.rows.clone().into_iter().rev().collect();
-        self.cols = self.cols.clone().into_iter().map(|s| rev(&s)).collect();
-    }
-
-    fn flip_h(&mut self) {
-        self.cols = self.cols.clone().into_iter().rev().collect();
-        self.rows = self.rows.clone().into_iter().map(|s| rev(&s)).collect();
     }
 }
 
@@ -173,8 +186,7 @@ fn rev(s: &str) -> String {
 
 struct Image {
     waters: String,
-    rows: Vec<String>,
-    cols: Vec<String>,
+    grid: Grid,
 }
 
 impl Image {
@@ -183,19 +195,21 @@ impl Image {
         for block in tiles.chunks(12) {
             for r in 1..9 {
                 for t in block {
-                    waters.push_str(&t.rows[r][1..9]);
+                    waters.push_str(&t.grid.rows[r][1..9]);
                 }
                 waters.push('\n');
             }
         }
-        let rows = vec![];
-        let cols = vec![];
 
-        Image { waters, rows, cols }
+        Image {
+            waters,
+            grid: Grid { rows: vec![], cols: vec![] },
+        }
     }
 
     fn count_roughness(&mut self) -> usize {
-        self.rows
+        self.grid
+            .rows
             .iter()
             .map(|x| x.chars().filter(|&c| c == '#').count())
             .sum::<usize>()
@@ -211,35 +225,19 @@ impl Image {
                 (rows, cols)
             },
         );
-        self.rows = rows;
-        self.cols = cols;
-    }
-
-    fn turn_left(&mut self) {
-        let new_cols: Vec<String> = self.rows.iter().map(|s| rev(s)).collect();
-        self.rows = self.cols.clone().into_iter().rev().collect();
-        self.cols = new_cols;
-    }
-
-    fn flip_v(&mut self) {
-        self.rows = self.rows.clone().into_iter().rev().collect();
-        self.cols = self.cols.clone().into_iter().map(|s| rev(&s)).collect();
-    }
-
-    fn flip_h(&mut self) {
-        self.cols = self.cols.clone().into_iter().rev().collect();
-        self.rows = self.rows.clone().into_iter().map(|s| rev(&s)).collect();
+        self.grid.rows = rows;
+        self.grid.cols = cols;
     }
 
     fn find_sea_monsters(&mut self) -> usize {
         let mut count = 0;
         let mut i = 0usize;
         while count == 0 {
-            for r in (2..self.rows.len()).rev() {
-                for x in 0..self.rows.len() - 20 {
-                    if BOT.is_match(&self.rows[r][x..x + 20])
-                        && MID.is_match(&self.rows[r - 1][x..x + 20])
-                        && TOP.is_match(&self.rows[r - 2][x..x + 20])
+            for r in (2..self.grid.rows.len()).rev() {
+                for x in 0..self.grid.rows.len() - 20 {
+                    if BOT.is_match(&self.grid.rows[r][x..x + 20])
+                        && MID.is_match(&self.grid.rows[r - 1][x..x + 20])
+                        && TOP.is_match(&self.grid.rows[r - 2][x..x + 20])
                     {
                         count += 1;
                     }
@@ -249,13 +247,13 @@ impl Image {
                 if i > 0 && i % 4 == 0 {
                     if i % 8 == 0 {
                         println!("{} flipping v", i);
-                        self.flip_v()
+                        self.grid.flip_v()
                     } else {
                         println!("{} flipping h", i);
-                        self.flip_h()
+                        self.grid.flip_h()
                     }
                 }
-                self.turn_left();
+                self.grid.turn_left();
             }
             i += 1;
         }
