4d86fc5a59c5ed03f459e4fdcdc41203330eb17d
Theodore Pauw <theodorepauw@outlook.com>
2021-01-13 20:48:40 +0200

    Nice! Didn't know about the Chinese Remainder Theorem. 🚏

--- src/twenty20/day13.rs
+++ src/twenty20/day13.rs
@@ -1,32 +1,33 @@
 const INPUT: &str = include_str!("./inputs/13.txt");
-use crate::util;
 use std::io::{self, Write};
 
-pub fn solve() -> util::Result<()> {
-    let lines: Vec<&str> = INPUT.lines().collect();
-    let timestamp = lines[0].parse::<usize>().unwrap();
-    let buses: &str = lines[1];
+lazy_static::lazy_static! {
+    static ref LINES: Vec<&'static str> = INPUT.lines().collect();
+    static ref TIMESTAMP: usize = LINES[0].parse::<usize>().unwrap();
+    static ref BUSES: &'static str = LINES[1];
+}
 
-    let (p1, p2) = (part1(buses, timestamp)?, part2(buses)?);
+pub fn solve() -> crate::util::Result<()> {
+    let (p1, p2) = (part1()?, part2()?);
     writeln!(io::stdout(), "Day 13 Part 1: {}\nDay 13 Part 2: {}", p1, p2)?;
     Ok(())
 }
 
-fn part1(buses: &str, timestamp: usize) -> util::Result<usize> {
-    buses
+fn part1() -> crate::util::Result<usize> {
+    BUSES
         .split_terminator(',')
         .filter(|&s| s != "x")
         .map(|s| {
             let id = s.parse::<usize>().unwrap();
-            (id, id - timestamp % id)
+            (id, id - *TIMESTAMP % id)
         })
         .min_by_key(|(_, wait)| *wait)
         .map(|(id, wait)| id * wait)
         .ok_or_else(|| "couldn't find minimum wait time for buses".into())
 }
 
-fn part2(buses: &str) -> util::Result<usize> {
-    let buses: Vec<(usize, usize)> = buses
+fn part2() -> crate::util::Result<usize> {
+    let buses: Vec<(usize, usize)> = BUSES
         .split_terminator(',')
         .enumerate()
         .filter(|&(_, bus)| bus != "x")
