4c87f0099282afaa9b55b7eaacf30573eb54f53b
Theodore Pauw <theodorepauw@outlook.com>
2021-01-19 00:55:31 +0200

    Much cleaner and faster. 🧬

--- src/twenty18/day05.rs
+++ src/twenty18/day05.rs
@@ -1,62 +1,35 @@
 const INPUT: &[u8] = include_bytes!("./inputs/05.txt");
-const CAP_DIFF: u8 = b'a' - b'A';
+const CAP_DIFF: u8 = b'a' - b'A'; // ASCII laid out such that this == b'a' b'A' == 32
 use crate::util::Result;
 use std::io::{self, Write};
 
 pub fn solve() -> Result<()> {
-    let react = |polymers: &[u8]| -> Result<Vec<bool>> {
-        let mut active = vec![true; polymers.len()];
-        let mut i = 0;
-        while let Some(j) = active[i + 1..]
-            .iter()
-            .position(|&is_active| is_active)
-            .map(|x| i + 1 + x)
-        {
-            let diff = if polymers[i] > polymers[j] {
-                polymers[i] - polymers[j]
-            } else {
-                polymers[j] - polymers[i]
-            };
+    let react = |polymers: &[u8]| -> Vec<u8> {
+        let mut stack = Vec::with_capacity(polymers.len());
 
-            if diff == CAP_DIFF {
-                active[i] = false;
-                active[j] = false;
-
-                if let Some(prev_active) = active[0..i].iter().rposition(|&is_active| is_active) {
-                    i = prev_active;
-                } else if let Some(fwd_offset) = if j + 1 < active.len() {
-                    active[j + 1..].iter().position(|&is_active| is_active)
-                } else {
-                    None
-                } {
-                    i += fwd_offset
-                } else {
-                    break;
-                }
+        for unit in polymers {
+            if stack.last().map_or(false, |&last| unit ^ last == CAP_DIFF) {
+                stack.pop();
             } else {
-                i = j;
-            }
-            if i == active.len() - 1 {
-                break;
+                stack.push(*unit);
             }
         }
-        Ok(active)
+
+        stack
     };
 
-    let reduced_len = |active: &[bool]| active.iter().filter(|&&is_active| is_active).count();
-    let still_active: Vec<bool> = react(INPUT)?;
-    let p1 = reduced_len(&still_active);
+    let reduced = react(INPUT);
+    let p1 = reduced.len();
     let p2 = (b'a'..=b'z')
         .map(|unit| {
-            (0..INPUT.len())
-                .filter(|&i| still_active[i])
-                .map(|i| INPUT[i])
-                .filter(|&x| !(x == unit || x == unit - CAP_DIFF))
+            reduced
+                .iter()
+                .filter(|&&x| !(x == unit || x == unit - CAP_DIFF))
+                .cloned()
                 .collect::<Vec<_>>()
         })
-        .filter_map(|p| react(&p).ok())
-        .map(|active| reduced_len(&active))
-        .min_by_key(|&len| len)
+        .map(|p| react(&p).len())
+        .min()
         .ok_or("no min polymer length")?;
 
     writeln!(io::stdout(), "Day 05 Part 1: {}\nDay 05 Part 2: {}", p1, p2)?;
