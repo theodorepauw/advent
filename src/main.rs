6eadf11a42936de501e91e24f49c2aea11093238
Theodore Pauw <theodorepauw@outlook.com>
2021-12-09 16:17:37 +0200

    Sloppy. Need to clean it up later. 🔋

--- src/main.rs
+++ src/main.rs
@@ -127,7 +127,7 @@ fn main() -> util::Result<()> {
                 (2020, 25) => twenty20::day25::solve(),
                 (2021, 1) => twenty21::day01::solve(),
                 (2021, 2) => twenty21::day02::solve(),
-                // (2020, 3) => twenty20::day03::solve(),
+                (2021, 3) => twenty21::day03::solve(),
                 // (2020, 4) => twenty20::day04::solve(),
                 // (2020, 5) => twenty20::day05::solve(),
                 // (2020, 6) => twenty20::day06::solve(),
---
+++ src/twenty21/day03.rs
@@ -0,0 +1,43 @@
+const INPUT: &str = include_str!("./inputs/03.txt");
+use std::{io::{self, Write}, usize};
+
+pub fn solve() -> crate::util::Result<()> {
+
+    let numbers: Vec<usize> = INPUT.lines().map(|s| usize::from_str_radix(s, 2).expect("not a binary number")).collect();
+    let binary_length: usize = INPUT.find('\n').expect("invalid input: no lines");
+
+    let most_common_bit = | list: &[usize], power: usize | {
+        let ones: usize = list.iter().map(|&n| (n>>power)&1).sum();
+        if ones + 1 > (list.len()+1>>1) { 1 } else { 0usize }
+    };
+
+    let (gamma, epsilon) = (0..binary_length).rev().fold((0, 0), |(g, e), i| {
+        let bit = most_common_bit(&numbers, i);
+        (g | bit<<i, e | (1-bit)<<i)
+    });
+
+
+    let p1 = gamma * epsilon;
+
+    let mut oxygen_generator_rating = numbers.clone();
+    let mut co2_scrubber_rating = numbers.clone();
+    let mut power = binary_length;
+
+    while oxygen_generator_rating.len() > 1 {
+        power = power.checked_sub(1).expect("no more options for ogr");
+        let bit = most_common_bit(&oxygen_generator_rating, power);
+        oxygen_generator_rating.retain(|&number| (number>>power)&1==bit);
+    }
+
+    let mut power = binary_length;
+    while co2_scrubber_rating.len() > 1 {
+        power = power.checked_sub(1).expect("no more options for ogr");
+        let bit = 1^most_common_bit(&co2_scrubber_rating, power);
+        co2_scrubber_rating.retain(|&number| (number>>power)&1 == bit);
+    }
+
+    let p2 = oxygen_generator_rating[0] * co2_scrubber_rating[0];
+
+    writeln!(io::stdout(), "Day 01 Part 1: {}\nDay 01 Part 2: {}", p1, p2)?;
+    Ok(())
+}
