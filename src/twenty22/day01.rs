d264560760cf65b54530dcfbdec95966d93823e0
Theodore Pauw <theodorepauw@icloud.com>
2022-12-01 10:38:27 +0200

    Rewrote w/o vec & sorting 🍓

--- src/twenty22/day01.rs
+++ src/twenty22/day01.rs
@@ -2,20 +2,25 @@ const INPUT: &str = include_str!("./inputs/01.txt");
 use std::io::{self, Write};
 
 pub fn solve() -> crate::util::Result<()> {
-    let mut elves_inventories: Vec<usize> = INPUT
-        .split("\n\n")
-        .map(|s| {
-            s.lines()
-                .map(|cals| cals.parse::<usize>().expect("cals not a number"))
-                .sum()
-        })
-        .collect();
+    let (mut most_cals, mut second_most_cals, mut third_most_cals) = (0, 0, 0);
+    for elf_calories in INPUT.split("\n\n").map(|s| {
+        s.lines()
+            .map(|cals| cals.parse::<usize>().expect("cals not a number"))
+            .sum::<usize>()
+    }) {
+        if elf_calories >= most_cals {
+            third_most_cals = second_most_cals;
+            second_most_cals = most_cals;
+            most_cals = elf_calories;
+        } else if elf_calories >= second_most_cals {
+            third_most_cals = second_most_cals;
+            second_most_cals = elf_calories;
+        } else if elf_calories > third_most_cals {
+            third_most_cals = elf_calories;
+        }
+    }
 
-    elves_inventories.sort_unstable();
-
-    let p1: &usize = elves_inventories.last().expect("elves' inventory empty");
-    let p2: usize = elves_inventories.iter().rev().take(3).sum();
-
+    let (p1, p2) = (most_cals, most_cals + second_most_cals + third_most_cals);
     writeln!(io::stdout(), "Day 01 Part 1: {}\nDay 01 Part 2: {}", p1, p2)?;
     Ok(())
 }
